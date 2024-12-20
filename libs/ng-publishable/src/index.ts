edff0f3c8b68f3d56e6c9c1d1a0468ef4612622b
Santosh Yadav <santosh.yadav198613@gmail.com>
2024-12-20 12:18:19 +0100

    add more components

--- libs/ng-publishable/src/index.ts
+++ libs/ng-publishable/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/ng-publishable/ng-publishable.component';
+
+export * from './lib/grid.component';
+
+export * from './lib/accordion.component';
---
+++ libs/ng-publishable/src/lib/accordion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-accordion',
+  imports: [CommonModule],
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.css',
+})
+export class AccordionComponent {}
